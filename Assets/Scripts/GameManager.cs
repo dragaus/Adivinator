fc08b9a1c8f852900052d290c9e1298beb72be00
francisco Castañeda <dragalichi@gmail.com>
2019-07-27 13:28:15 -0500

    Adivinator

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    GameObject genie;
+    GameObject message;
+
+    Button yesButton;
+    Button noButton;
+    Text talkText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        genie = GameObject.Find("Genie");
+        message = GameObject.Find("Message");
+        genie.SetActive(false);
+        message.SetActive(false);
+
+        yesButton = GameObject.Find("Yes Button").GetComponent<Button>();
+        noButton = GameObject.Find("No Button").GetComponent<Button>();
+
+        yesButton.gameObject.SetActive(false);
+        noButton.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayTheGame()
+    {
+        Debug.Log("Play the game");
+    }
+}
